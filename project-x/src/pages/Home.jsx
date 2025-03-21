801c3663403f77fe11e6bd78ef8ccb57a268a48c
Elio Dredd <143877390+Ellio-Dredd@users.noreply.github.com>
2025-03-21 15:46:54 +0530

    Updated the Home Page UI

--- project-x/src/pages/Home.jsx
+++ project-x/src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import CoverImage from "../components/CoverImage";
+
+
 export default function Home() {
     return (
       <div>
+        <CoverImage/>
+        
         <h1>Welcome to Rajapakshe Pharmacy</h1>
         <p>Shop for medicines and book doctor appointments.</p>
       </div>
