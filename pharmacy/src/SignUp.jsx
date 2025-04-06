385fcd115bc47ae03189a038ce25d81fc5ad72cd
Cheni-SllitA <165175459+Cheni-SllitA@users.noreply.github.com>
2025-04-06 22:48:33 +0530

    Update SignUp.jsx

--- pharmacy/src/SignUp.jsx
+++ pharmacy/src/SignUp.jsx
@@ -26,8 +26,16 @@ export default function SignUp() {
     });
 
     const handleChange = (e) => {
-        const { id, value } = e.target;
-        setFormData({ ...formData, [id]: value });
+        const { id, name, value } = e.target;
+        setFormData({ ...formData, [id || name]: value });
+    };
+
+    const handleAgeUnitChange = (e) => {
+        const { name, checked } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            ageUnit: { ...prev.ageUnit, [name]: checked }
+        }));
     };
 
     const handleSubmit = async (e) => {
@@ -39,7 +47,7 @@ export default function SignUp() {
         }
 
         try {
-            const response = await axios.post("http://localhost:5173/api/auth/signup", {
+            const response = await axios.post("http://localhost:5000/api/auth/signup", {
                 firstName: formData.firstName,
                 lastName: formData.lastName,
                 email: formData.email,
@@ -61,6 +69,7 @@ export default function SignUp() {
                 ageUnit: { years: false, months: false, weeks: false }
             });
         } catch (error) {
+            console.error("Signup error:", error); // 👈 Logs full error to browser console
             alert(error.response?.data?.message || "Signup failed!");
         }
     };
@@ -81,31 +90,110 @@ export default function SignUp() {
                 backgroundColor: "#fff"
             }}
         >
-            <TextField id="firstName" label="First Name" onChange={handleChange} fullWidth required />
-            <TextField id="lastName" label="Last Name" onChange={handleChange} fullWidth required />
-            <TextField id="email" label="Email" type="email" onChange={handleChange} fullWidth required />
+            <TextField
+                id="firstName"
+                label="First Name"
+                value={formData.firstName}
+                onChange={handleChange}
+                fullWidth
+                required
+            />
+            <TextField
+                id="lastName"
+                label="Last Name"
+                value={formData.lastName}
+                onChange={handleChange}
+                fullWidth
+                required
+            />
+            <TextField
+                id="email"
+                label="Email"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
+                fullWidth
+                required
+            />
 
             <FormControl>
                 <FormLabel>Gender</FormLabel>
-                <RadioGroup id="gender" onChange={handleChange}>
+                <RadioGroup
+                    name="gender"
+                    value={formData.gender}
+                    onChange={handleChange}
+                    row
+                >
                     <FormControlLabel value="male" control={<Radio />} label="Male" />
                     <FormControlLabel value="female" control={<Radio />} label="Female" />
+                    <FormControlLabel value="other" control={<Radio />} label="Other" />
                 </RadioGroup>
             </FormControl>
 
-            <TextField id="age" label="Age" type="number" onChange={handleChange} fullWidth required />
+            <TextField
+                id="age"
+                label="Age"
+                type="number"
+                value={formData.age}
+                onChange={handleChange}
+                fullWidth
+                required
+            />
 
             <FormControl>
                 <FormLabel>Age Unit</FormLabel>
-                <FormGroup>
-                    <FormControlLabel control={<Checkbox />} label="Years" />
-                    <FormControlLabel control={<Checkbox />} label="Months" />
-                    <FormControlLabel control={<Checkbox />} label="Weeks" />
+                <FormGroup row>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                name="years"
+                                checked={formData.ageUnit.years}
+                                onChange={handleAgeUnitChange}
+                            />
+                        }
+                        label="Years"
+                    />
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                name="months"
+                                checked={formData.ageUnit.months}
+                                onChange={handleAgeUnitChange}
+                            />
+                        }
+                        label="Months"
+                    />
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                name="weeks"
+                                checked={formData.ageUnit.weeks}
+                                onChange={handleAgeUnitChange}
+                            />
+                        }
+                        label="Weeks"
+                    />
                 </FormGroup>
             </FormControl>
 
-            <TextField id="password" label="Password" type="password" onChange={handleChange} fullWidth required />
-            <TextField id="confirmPassword" label="Confirm Password" type="password" onChange={handleChange} fullWidth required />
+            <TextField
+                id="password"
+                label="Password"
+                type="password"
+                value={formData.password}
+                onChange={handleChange}
+                fullWidth
+                required
+            />
+            <TextField
+                id="confirmPassword"
+                label="Confirm Password"
+                type="password"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                fullWidth
+                required
+            />
 
             <Button variant="contained" color="primary" type="submit">
                 Sign Up
