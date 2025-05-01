e424f33fed2c8ad8634ceee28c74163a8d23b099
Elio Dredd <143877390+Ellio-Dredd@users.noreply.github.com>
2025-05-01 23:23:16 +0530

    now the sessions works

--- Server/models/CartItem.js
+++ Server/models/CartItem.js
@@ -5,6 +5,7 @@ const cartItemSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     image: { type: String, required: true},
+    userID: { type: String, required: true},
     quantity: { type: Number, required: true }
 
 }, { timestamps: true, collection: 'cartitems' });  
